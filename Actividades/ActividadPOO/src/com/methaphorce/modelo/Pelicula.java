0ad029c18a1e7cf9b0e68343fb953d1ed69353ed
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-09-18 14:32:57 -0600

    Creando paquetes y clase pelicula

---
+++ Actividades/ActividadPOO/src/com/methaphorce/modelo/Pelicula.java
@@ -0,0 +1,8 @@
+package com.methaphorce.modelo;
+
+public class Pelicula {
+    private int id;
+    private String name;
+    private boolean isAvailable;
+    
+}
--- Actividades/ActividadPOO/src/com/methaphorce/vista/Main.java
+++ Actividades/ActividadPOO/src/com/methaphorce/vista/Main.java
@@ -1,3 +1,5 @@
+package com.methaphorce.vista;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
