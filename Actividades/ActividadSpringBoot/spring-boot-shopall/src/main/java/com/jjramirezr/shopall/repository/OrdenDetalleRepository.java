c690800666cbd6b6d7ac3086edeb18662416b9e7
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-10-10 17:55:49 -0600

    Creacion Repositories

---
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/repository/OrdenDetalleRepository.java
@@ -0,0 +1,7 @@
+package com.jjramirezr.shopall.repository;
+
+import com.jjramirezr.shopall.data.OrdenDetalle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdenDetalleRepository extends JpaRepository<OrdenDetalle,Integer> {
+}
