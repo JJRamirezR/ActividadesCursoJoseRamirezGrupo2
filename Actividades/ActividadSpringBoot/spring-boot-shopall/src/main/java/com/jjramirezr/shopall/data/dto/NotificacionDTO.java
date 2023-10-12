b7712d7d704308cf0d0e61a88d746f5e3a54e192
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-10-12 17:42:52 -0600

    Renombre notifiaciones DTO

--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/data/dto/NotificacionDTO.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/data/dto/NotificacionDTO.java
@@ -1,9 +1,14 @@
 package com.jjramirezr.shopall.data.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 public class NotificacionClienteDTO {
-    private ClienteDTO cliente;
+    //private ClienteDTO cliente;
     private String Notificacion;
     private Timestamp fecha;
 }
--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/data/dto/NotificacionListaDTO.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/data/dto/NotificacionListaDTO.java
@@ -3,7 +3,6 @@ package com.jjramirezr.shopall.data.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -12,5 +11,5 @@ public class ClienteNotificacionDTO {
     //clase para mostrar las notificaciones
     private String nombre;
     private String email;
-    private List<NotificacionClienteDTO> notificacionesDTO;
+    private List<NotificacionDTO> notificacionesDTO;
 }
