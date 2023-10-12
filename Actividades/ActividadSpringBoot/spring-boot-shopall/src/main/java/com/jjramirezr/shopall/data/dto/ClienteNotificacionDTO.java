cb1c753d4c5b63e585905e3c31b2d1424307ebc0
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-10-11 21:28:42 -0600

    agregando dto auxiliar

---
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/data/dto/ClienteNotificacionDTO.java
@@ -0,0 +1,16 @@
+package com.jjramirezr.shopall.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class ClienteNotificacionDTO {
+    //clase para mostrar las notificaciones
+    private String nombre;
+    private String email;
+    private List<NotificacionClienteDTO> notificacionesDTO;
+}
