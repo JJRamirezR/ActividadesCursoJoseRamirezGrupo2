fe32471f68bf782d1d45b2f9daaae6deeedc34e7
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-10-19 11:30:02 -0600

    Modificacion controladores

--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/controller/InventarioController.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/controller/InventarioController.java
@@ -1,2 +1,21 @@
-package com.jjramirezr.shopall.controller;public class InventarioController {
+package com.jjramirezr.shopall.controller;
+
+import com.jjramirezr.shopall.data.dto.InventarioCreationDTO;
+import com.jjramirezr.shopall.service.InventarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/inventario")
+public class InventarioController {
+    @Autowired
+    private InventarioService inventarioService;
+
+    //Crear un nuevo producto
+    @PostMapping("/nuevo/{vendedorId}")
+    public InventarioCreationDTO addProducto(@PathVariable int vendedorId,
+                                             @RequestBody InventarioCreationDTO nuevoDTO){
+        return inventarioService.saveProducto(vendedorId,nuevoDTO);
+
+    }
 }
--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/controller/VendedorController.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/controller/VendedorController.java
@@ -1,2 +1,33 @@
-package com.jjramirezr.shopall.controller;public class VendedorController {
+package com.jjramirezr.shopall.controller;
+
+import com.jjramirezr.shopall.data.dto.NotificacionListaDTO;
+import com.jjramirezr.shopall.data.dto.UsuarioDTO;
+import com.jjramirezr.shopall.data.dto.VendedorCreationDTO;
+import com.jjramirezr.shopall.service.VendedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendedor")
+public class VendedorController {
+    @Autowired
+    private VendedorService vendedorService;
+
+    @GetMapping("/lista")
+    public List<UsuarioDTO> findAll(){return vendedorService.findAll();}
+    @GetMapping("/{vendedorId}/notificaciones")
+    public NotificacionListaDTO getNotificaciones(@PathVariable int vendedorId){
+        return vendedorService.getNotificaciones(vendedorId);
+    }
+
+    @PostMapping("/nuevo")
+    public VendedorCreationDTO addVendedor(@RequestBody VendedorCreationDTO vendedorDTO){
+        return vendedorService.saveVendedor(vendedorDTO);
+    }
+
+
+
+
 }
