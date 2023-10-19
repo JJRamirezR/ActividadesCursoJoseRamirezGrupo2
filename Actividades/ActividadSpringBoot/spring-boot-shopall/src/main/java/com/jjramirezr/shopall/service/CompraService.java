c23af27572889844b2223af085169148f098666f
JJRamirezR <70038057+JJRamirezR@users.noreply.github.com>
2023-10-19 11:34:10 -0600

    Modificacion de métodos en capa de servicios

--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/service/CompraService.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/service/CompraService.java
@@ -1,2 +1,54 @@
-package com.jjramirezr.shopall.service;public class CompraService {
+package com.jjramirezr.shopall.service;
+
+import com.jjramirezr.shopall.data.Cliente;
+import com.jjramirezr.shopall.data.Compra;
+import com.jjramirezr.shopall.data.Inventario;
+import com.jjramirezr.shopall.data.Orden;
+import com.jjramirezr.shopall.data.dto.CompraCreationDTO;
+import com.jjramirezr.shopall.repository.ClienteRepository;
+import com.jjramirezr.shopall.repository.CompraRepository;
+import com.jjramirezr.shopall.repository.InventarioRepository;
+import com.jjramirezr.shopall.repository.OrdenRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompraService {
+
+    @Autowired
+    CompraRepository compraRepository;
+    @Autowired
+    OrdenRepository ordenRepository;
+    @Autowired
+    ClienteRepository clienteRepository;
+    @Autowired
+    InventarioRepository inventarioRepository;
+
+    @Transactional
+    public List<CompraCreationDTO> saveCompra(int clienteId, List<CompraCreationDTO> listaDTO) {
+
+        Cliente cliente = clienteRepository.getReferenceById(clienteId);
+        Orden orden = new Orden(cliente, "Creada");
+        ordenRepository.save(orden);
+        System.out.println(orden);
+        double total = 0.0;
+        for(CompraCreationDTO productoDTO: listaDTO){
+            Inventario producto = inventarioRepository.getReferenceById(productoDTO.getIdInventario());
+            int cantidad = productoDTO.getCantidad();
+            double precio = cantidad * producto.getPrecioUnitario();
+            total +=precio;
+            compraRepository.save(new Compra(orden,producto,
+                    cantidad, precio));
+
+        }
+        orden.setTotal(total);
+        ordenRepository.save(orden);
+
+
+        return listaDTO;
+    }
+
 }
--- Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/service/InventarioService.java
+++ Actividades/ActividadSpringBoot/spring-boot-shopall/src/main/java/com/jjramirezr/shopall/service/InventarioService.java
@@ -1,2 +1,36 @@
-package com.jjramirezr.shopall.service;public class InventarioService {
+package com.jjramirezr.shopall.service;
+
+import com.jjramirezr.shopall.data.CategoriaProducto;
+import com.jjramirezr.shopall.data.Inventario;
+import com.jjramirezr.shopall.data.Vendedor;
+import com.jjramirezr.shopall.data.dto.InventarioCreationDTO;
+import com.jjramirezr.shopall.repository.CategoriaProductoRepository;
+import com.jjramirezr.shopall.repository.InventarioRepository;
+import com.jjramirezr.shopall.repository.VendedorRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventarioService {
+
+    @Autowired
+    InventarioRepository inventarioRepository;
+    @Autowired
+    VendedorRepository vendedorRepository;
+
+    @Autowired
+    CategoriaProductoRepository categoriaProductoRepository;
+
+    @Transactional
+    public InventarioCreationDTO saveProducto(int vendedorId, InventarioCreationDTO nuevoDTO) {
+        Vendedor vendedor = vendedorRepository.getReferenceById(vendedorId);
+        CategoriaProducto categoria = categoriaProductoRepository.getReferenceById(nuevoDTO.getCategoria());
+        inventarioRepository.save(new Inventario(vendedor,categoria,
+                nuevoDTO.getNombre(), nuevoDTO.getDescripcion(), nuevoDTO.getUnidad(), nuevoDTO.getStock(),
+                nuevoDTO.getPrecioUnitario()));
+
+        return  nuevoDTO;
+
+    }
 }
